d9d3678677e255a4361dff88cdfdf47323aec19f
Gomez Maximiliano <maxil5gomez@gmail.com>
2023-10-29 21:29:21 -0300

    Se agrego spinner con setTimeout temporal

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,3 +1,4 @@
+import Spinner from "react-bootstrap/Spinner";
 import { useEffect, useState } from "react";
 import Item from "../Item/Item";
 import "./styles.css";
@@ -7,19 +8,21 @@ const ItemList = () => {
   const [productSelectedId, setProductSelectedId] = useState(1);
 
   const fetchProducts = () => {
-    fetch("https://6539b0efe3b530c8d9e89779.mockapi.io/products", {
-      method: "GET",
-    })
-      .then((res) => {
-        return res.json();
+    setTimeout(() => {
+      fetch("https://6539b0efe3b530c8d9e89779.mockapi.io/products", {
+        method: "GET",
       })
-      .then((data) => {
-        setProductList(data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setProductList(data);
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 2000);
   };
 
   const getProduct = () => {
@@ -36,7 +39,11 @@ const ItemList = () => {
   useEffect(() => {
     fetchProducts();
   }, []);
-  return (
+  return productList.length === 0 ? (
+    <Spinner animation="border" role="status" size="xl" className="spinner">
+      <span className="visually-hidden ">Loading...</span>
+    </Spinner>
+  ) : (
     <div className="card__container">
       {productList.map(({ name, description, price, image, id }, index) => (
         <div
