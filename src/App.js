8c6a31b738610d4d70b42e5859caf0b4a64f0e77
Gomez Maximiliano <maxil5gomez@gmail.com>
2023-11-04 18:26:58 -0300

    Se importaron los elementos para empezar a trabajar con rutas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemListContainer from "./pages/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./pages/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <ItemListContainer greeting="Aqui ira el contenedor de los productos de la tienda" />
-    </div>
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:id" element={<ItemListContainer />} />
+        <Route path="/item/:id" element={<ItemDetailContainer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
