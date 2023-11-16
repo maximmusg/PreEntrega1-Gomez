4b9dafd810a47d98c2a43b9db4a67a8b6163b579
Gomez Maximiliano <maxil5gomez@gmail.com>
2023-11-16 20:51:34 -0300

    Se agregaron algunos estilos al avtive en navBar , se cambio Lnk por NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,63 @@
 import CartWidget from "../CartWidget/CartWidget";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./styles.css";
 
 const Navbar = () => {
   return (
     <div className="navbar__container">
       <div>
-        <Link to={"/"} className="category__btn">
+        <NavLink to={"/"} className="category__btn">
           {/* <img src={"./logo.png"} alt="logo" className="image__navbar" /> */}
           <h1>mmsg Aromas</h1>
-        </Link>
+        </NavLink>
       </div>
 
       <div>
         <ul className="list__container">
           <li className="item__list">
-            <Link to={"/"} className="category__btn btn__home">
+            <NavLink
+              activeclassname="active"
+              to={"/"}
+              className="category__btn btn__home"
+            >
               Home
-            </Link>
+            </NavLink>
           </li>
           <li className="item__list">
-            <Link to={"/category/electronics"} className="category__btn">
+            <NavLink
+              activeclassname="active"
+              to={"/category/electronics"}
+              className="category__btn"
+            >
               Electronicos
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link to={"/category/jewelery"} className="category__btn">
+            <NavLink
+              activeclassname="active"
+              to={"/category/jewelery"}
+              className="category__btn"
+            >
               Joyas
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link to={"/category/men's-clothing"} className="category__btn">
+            <NavLink
+              activeclassname="active"
+              to={"/category/men's-clothing"}
+              className="category__btn"
+            >
               Ropa de hombre
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link to={"/category/women's-clothing"} className="category__btn">
+            <NavLink
+              activeclassname="active"
+              to={"/category/women's-clothing"}
+              className="category__btn"
+            >
               Ropa de Mujer
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </div>
